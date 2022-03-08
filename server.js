e933fa5eef7a2428485116159756ee6cb54b7962
Megan Campbell <megancampbell201@gmail.com>
2022-03-08 13:04:03 -0500

    added route to query db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,7 +8,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: '127.0.0.1',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'election',
+      port: '3306'
+    },
+    console.log('Connected to the election database.')
+  );
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
   // Default response for any other request (Not Found)
 app.use((req, res) => {
