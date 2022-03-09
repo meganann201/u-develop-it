aa80fbf1fae8acd71a0b6f6a1ef7635a93a569c2
Megan Campbell <megancampbell201@gmail.com>
2022-03-09 12:52:12 -0500

    reorganized project structure

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: '127.0.0.1',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'election',
+      port: '3306'
+    },
+    console.log('Connected to the election database.')
+  );
+
+  module.exports = db;
